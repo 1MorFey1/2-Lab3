2b67f9fb9fcf53e7f54107c8f9fcc2c6a3db960b
1MorFey1 <dumdeedum389@gmail.com>
2025-06-13 22:36:56 +0700

    fix lab3-2

--- Lab3-2/Lab3-2.cpp
+++ Lab3-2/Lab3-2.cpp
@@ -1,6 +1,47 @@
 ﻿#include <iostream>
+#include "Animal.h"
+#include "Bird.h"
+#include "FlyingBird.h"
+#include "Fish.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Animal* test = new Bird();
+	test->eat();
+	test->breathe();
+
+	Animal* animal = NULL;
+
+	char animal_char;
+	cout << "Input animal type (b-bird, f-fish, y-flyingBird): ";
+	cin >> animal_char;
+
+	switch (animal_char)
+	{
+	case 'b':
+
+		animal = new Bird();
+		break;
+
+	case 'f':
+
+		animal = new Fish();
+		break;
+
+	case 'y':
+
+		animal = new FlyingBird();
+		break;
+
+	default:
+		cout << "Enter Animal type:\n";
+		return -1;
+	}
+
+	animal->breathe();
+	animal->eat();
+
+	return 0;
 }
